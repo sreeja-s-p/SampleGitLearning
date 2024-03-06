58c8e92a594ba18db0e748107e9fa94dc4b0ce20
sreeja-s-p <sreejapanicker04@gmail.com>
2024-03-06 13:14:28 +0530

    edited file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello everyone! How are you doing?");
+        System.out.println("Hello everyone! How are you doing? Example");
     }
 }
